8b723fe7349c8b0a2fb90538ed49fad3f50de998
Jean Perbet <jeanperbet@icloud.com>
2025-09-09 23:17:56 +0200

    feat: add vercel web analytics

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-    <UApp>
-        <NuxtPage />
-    </UApp>
+    <Analytics />
+    <NuxtPage />
 </template>
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>

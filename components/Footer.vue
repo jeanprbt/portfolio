7971061e89f8749e5cbb9fa7614f512a98020656
Jean Perbet <jeanperbet@icloud.com>
2025-11-03 18:36:35 +0100

    fix: smooth color transition on footer background

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="text-primary bg-secondary font-text flex items-center justify-center">
+    <footer class="text-primary bg-secondary font-text flex items-center justify-center transition-colors duration-500">
         <div class="px-5 py-2 mb-3 border-1 md:border-2 border-highlight rounded-full">
             <p class="text-xs sm:text-sm lg:text-base">&copy; 2025 Jean Perbet. All rights reserved.</p>
         </div>
